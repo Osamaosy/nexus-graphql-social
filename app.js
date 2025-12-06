e251819eea2e8ea7767af61051d6c00dc5a42fe7
Osama Ibrahim <osarezk119@gmail.com>
2025-12-06 02:45:39 +0200

    feat: setup socket.io helper for shared instance

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ mongoose
     'mongodb://127.0.0.1:27017/messages'
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
